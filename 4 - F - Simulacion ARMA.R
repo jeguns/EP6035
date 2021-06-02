e19e2bf69d7d46b20b4f9b0201d06313fe3fc4ae
Jesus Eduardo Gamboa Unsihuay <jgamboa@lamolina.edu.pe>
2021-06-01 21:10:08 -0500

    Actualización 01/06

---
+++ 4 - F - Simulacion ARMA.R
@@ -0,0 +1,69 @@
+
+
+library(TSA)
+library(ggfortify)
+
+# ARIMA(1,0,1) ------------------------------------------------------------
+
+phi    = 0.88
+theta  = 0.91
+sigma  = 2.5
+
+set.seed(564654)
+arima.sim(list(order = c(1,0,1), ar = phi, ma = theta), n = 150) -> Y1
+
+Y1 %>% 
+  autoplot(type="l") + 
+  geom_hline(yintercept = 0)
+
+Y1 %>% aTSA::stationary.test(method = "kpss",lag.short=T)
+Y1 %>% aTSA::stationary.test(method = "kpss",lag.short=F)
+Y1 %>% aTSA::stationary.test(method = "adf")
+Y1 %>% aTSA::stationary.test(method = "pp",lag.short=F)
+Y1 %>% aTSA::stationary.test(method = "pp",lag.short=T)
+Y1 %>% BoxCox.lambda()
+Y1 %>% archTest()
+Y1 %>% McLeod.Li.test(y=.)
+
+Y1 %>% 
+  TSA::acf(type="correlation", lag = 50, plot = TRUE, main = "Función de autocorrelación")
+
+Y1 %>% 
+  TSA::acf(type="partial", lag = 50, plot = TRUE, main = "Función de autocorrelación parcial")
+
+# ARIMA(2,1,2) ------------------------------------------------------------
+
+phi    = c(0.65,0.2)
+theta  = c(0.4,-0.9)
+sigma  = 1
+
+set.seed(489)
+arima.sim(list(order = c(2,2,2), ar = phi, ma = theta), n = 150) -> Y2
+
+Y2 %>% 
+  autoplot(type="l") 
+
+Y2 %>% aTSA::stationary.test(method = "kpss",lag.short=T)
+Y2 %>% aTSA::stationary.test(method = "kpss",lag.short=F)
+Y2 %>% aTSA::stationary.test(method = "adf")
+Y2 %>% aTSA::stationary.test(method = "pp",lag.short=F)
+Y2 %>% aTSA::stationary.test(method = "pp",lag.short=T)
+
+Y2 %>% diff %>% aTSA::stationary.test(method = "kpss",lag.short=T)
+Y2 %>% diff %>% aTSA::stationary.test(method = "kpss",lag.short=F)
+Y2 %>% diff %>% aTSA::stationary.test(method = "adf")
+Y2 %>% diff %>% aTSA::stationary.test(method = "pp",lag.short=F)
+Y2 %>% diff %>% aTSA::stationary.test(method = "pp",lag.short=T)
+
+Y2 %>% diff %>% diff %>% aTSA::stationary.test(method = "kpss",lag.short=T)
+Y2 %>% diff %>% diff %>% aTSA::stationary.test(method = "kpss",lag.short=F)
+Y2 %>% diff %>% diff %>% aTSA::stationary.test(method = "adf")
+Y2 %>% diff %>% diff %>% aTSA::stationary.test(method = "pp",lag.short=F)
+Y2 %>% diff %>% diff %>% aTSA::stationary.test(method = "pp",lag.short=T)
+
+Y2 %>% diff %>% diff %>%
+  TSA::acf(type="correlation", lag = 50, plot = TRUE, main = "Función de autocorrelación")
+
+Y2 %>% diff %>% diff %>%
+  TSA::acf(type="partial", lag = 50, plot = TRUE, main = "Función de autocorrelación parcial")
+
