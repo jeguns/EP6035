d9155cb182d9a2a735f7355d38fdb6debbdaa8fc
Jesus Eduardo Gamboa Unsihuay <jgamboa@lamolina.edu.pe>
2021-06-02 20:09:45 -0500

    Actualización 02/06

--- 4 - D- Simulacion MA.R
+++ 4 - D- Simulacion MA.R
@@ -7,14 +7,15 @@ library(ggfortify)
 # ARIMA(0,0,1) con theta = 0.72
 
 # Y_t = e_t - 0.72e_{t-1}
+
 # Y_t = e_t + (-0.72)e_{t-1}
 
 theta  = -0.72
 sigma  = 3.4
 
 set.seed(315)
-arima.sim(model=list(ar=c(theta)), n=150, sd = sigma) -> Y.MA1A 
-arima.sim(list(order = c(0,0,1), ma = theta), n = 150) -> Y.MA1A # AR(1) con phi = 0.85
+arima.sim(model=list(ma=c(theta)), n=150, sd = sigma) -> Y.MA1A 
+arima.sim(list(order = c(0,0,1), ma = c(theta)), n = 150, sd = sigma) -> Y.MA1A # AR(1) con phi = 0.85
 
 Y.MA1A %>% 
   autoplot(type="l") + 
@@ -57,21 +58,40 @@ Y.IMA1A %>% archTest()
 Y.IMA1A %>% McLeod.Li.test(y=.)
 
 Y.IMA1A %>% 
-  TSA::acf(type="correlation", lag = 50, plot = TRUE, main = "Función de autocorrelación")
+  TSA::acf(type="correlation", lag = 50, 
+           plot = TRUE, main = "Función de autocorrelación")
 
 Y.IMA1A %>% 
-  TSA::acf(type="partial", lag = 50, plot = TRUE, main = "Función de autocorrelación parcial")
+  TSA::acf(type="partial", lag = 50, 
+           plot = TRUE, main = "Función de autocorrelación parcial")
+
+Y.IMA1A %>% diff %>% plot
+Y.IMA1A %>% diff %>% aTSA::stationary.test(method = "kpss",lag.short=T)
+Y.IMA1A %>% diff %>% aTSA::stationary.test(method = "kpss",lag.short=F)
+Y.IMA1A %>% diff %>% aTSA::stationary.test(method = "adf")
+Y.IMA1A %>% diff %>% aTSA::stationary.test(method = "pp",lag.short=F)
+Y.IMA1A %>% diff %>% aTSA::stationary.test(method = "pp",lag.short=T)
+Y.IMA1A %>% diff %>% BoxCox.lambda()
+Y.IMA1A %>% diff %>% archTest()
+Y.IMA1A %>% diff %>% McLeod.Li.test(y=.)
 
 Y.IMA1A %>% diff %>% 
-  TSA::acf(type="correlation", lag = 50, plot = TRUE, main = "Función de autocorrelación")
+  TSA::acf(type="correlation", lag = 50, 
+           plot = TRUE, main = "Función de autocorrelación")
 
 Y.IMA1A %>% diff %>% 
-  TSA::acf(type="partial", lag = 50, plot = TRUE, main = "Función de autocorrelación parcial")
-
-
+  TSA::acf(type="partial", lag = 50, 
+           plot = TRUE, main = "Función de autocorrelación parcial")
 
 library(writexl)
-writexl::write_xlsx(x = data.frame(Y = Y.IMA1A), path = "Serie_simulada.xlsx")
+writexl::write_xlsx(x    = data.frame(Y = Y.IMA1A), 
+                    path = "Serie_simulada.xlsx")
 
 
+# ARIMA(2,1,1) con phi1 = 0.7, phi2 = -0.18 y theta = 0.7 --------------------------------
+
+arima.sim(list(order = c(2,1,1), 
+               ar    = c(0.7,-0.18),
+               ma    = c(-0.7)), 
+          n = 150) -> serie2
 
