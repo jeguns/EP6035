4649c520b2db1347dc9d78c018242e6cb81a4f02
Jesus Eduardo Gamboa Unsihuay <jgamboa@lamolina.edu.pe>
2021-06-16 18:37:26 -0500

    Actualización 16/06

--- 4 - H - Diferenciacion estacional.R
+++ 4 - H - Diferenciacion estacional.R
@@ -12,7 +12,7 @@ serie1 %>%
   autoplot(ts.geom = "ribbon", ts.fill="dodgerblue2", alpha = 0.5) + theme_void()
 
 serie1 %>% 
-  window(start = c(2018,1)) %>% 
+  window(start = c(2010,7)) %>% 
   autoplot(ts.geom = "ribbon", ts.fill="dodgerblue2", alpha = 0.5) + theme_void()
 
 serie1 %>% aTSA::stationary.test(method = "kpss",lag.short=T)
@@ -28,10 +28,10 @@ serie1 %>% diff %>% aTSA::stationary.test(method = "adf")
 serie1 %>% diff %>% aTSA::stationary.test(method = "pp",lag.short=F)
 serie1 %>% diff %>% aTSA::stationary.test(method = "pp",lag.short=T)
 serie1 %>% diff %>% BoxCox.lambda()
+serie1 %>% diff %>% acf2(240) # (1,1,1)x(1,0,0)
 boxplot(diff(serie1) ~ cycle(diff(serie1)))
 serie1 %>% diff %>% monthplot()
 serie1 %>% diff %>% kruskal.test(cycle(diff(serie1)))
-serie1 %>% diff %>% acf2(240)
 
 serie1 %>% diff %>% diff(12) %>% aTSA::stationary.test(method = "kpss",lag.short=T)
 serie1 %>% diff %>% diff(12) %>% aTSA::stationary.test(method = "kpss",lag.short=F)
@@ -42,6 +42,6 @@ serie1 %>% diff %>% diff(12) %>% BoxCox.lambda()
 boxplot(diff(diff(serie1),12) ~ cycle(diff(diff(serie1),12)))
 serie1 %>% diff %>% diff(12) %>% monthplot()
 serie1 %>% diff %>% diff(12) %>% kruskal.test(cycle(diff(diff(serie1),12)))
-serie1 %>% diff %>% diff(12) %>% acf2(240)
+serie1 %>% diff %>% diff(12) %>% acf2(240) # (1,1,1)x(0,1,1)
 
 auto.arima(serie1)
